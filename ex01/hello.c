e4f31642f5f64e3412e0c8feb4b20ef43af5b450
wchenx <2868466904@qq.com>
2026-06-04 19:45:56 +0800

    feat: 增加键盘输入交互功能，更新 README

--- ex01/hello.c
+++ ex01/hello.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 int main() {
+    char name[100];
+    
     printf("Hello, World!\n");
+    
+    printf("请输入你的名字: ");
+    scanf("%s", name);
+    
+    printf("你好, %s! 欢迎学习 GitHub。\n", name);
+    
     return 0;
 }
